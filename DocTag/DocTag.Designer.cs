68f41867b38ffdeab58e0fb50f3464f9dc543f42
xland <412588801@qq.com>
2014-11-29 13:24:52 +0800

    文档增加标签功能

--- DocTag/DocTag.Designer.cs
+++ DocTag/DocTag.Designer.cs
@@ -28,19 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.TagTB = new System.Windows.Forms.TextBox();
             this.TagWP = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.TagTB);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 26);
+            this.panel1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(365, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "增加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SaveTagBtn_Click);
             // 
             // TagTB
             // 
-            this.TagTB.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TagTB.Dock = System.Windows.Forms.DockStyle.Fill;
             this.TagTB.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.TagTB.Location = new System.Drawing.Point(6, 6);
+            this.TagTB.Location = new System.Drawing.Point(0, 0);
             this.TagTB.Multiline = true;
             this.TagTB.Name = "TagTB";
-            this.TagTB.Size = new System.Drawing.Size(440, 26);
-            this.TagTB.TabIndex = 1;
+            this.TagTB.Size = new System.Drawing.Size(365, 26);
+            this.TagTB.TabIndex = 3;
             this.TagTB.TextChanged += new System.EventHandler(this.TagTB_TextChanged);
             // 
             // TagWP
@@ -49,7 +74,7 @@
             this.TagWP.Location = new System.Drawing.Point(6, 32);
             this.TagWP.Name = "TagWP";
             this.TagWP.Size = new System.Drawing.Size(440, 309);
-            this.TagWP.TabIndex = 5;
+            this.TagWP.TabIndex = 4;
             // 
             // DocTag
             // 
@@ -57,19 +82,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(452, 347);
             this.Controls.Add(this.TagWP);
-            this.Controls.Add(this.TagTB);
+            this.Controls.Add(this.panel1);
             this.Name = "DocTag";
             this.Padding = new System.Windows.Forms.Padding(6);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "增加标签";
             this.Load += new System.EventHandler(this.DocTagWin_Loaded);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox TagTB;
         private System.Windows.Forms.FlowLayoutPanel TagWP;
     }
---
+++ DocTag/TagSuggestionCtl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocTag.Entity;
+using DocTag.Helper;
+
+namespace DocTag
+{
+    public partial class TagSuggestionCtl : UserControl
+    {
+        public TagSuggestionCtl()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void CloseSuggestionBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        public void ClearTags()
+        {
+            TagSuggestionFLP.Controls.Clear();
+        }
+        public void AddTag(LinkLabel tagLabel)
+        {
+            TagSuggestionFLP.Controls.Add(tagLabel);
+        }
+
+    }
+}
